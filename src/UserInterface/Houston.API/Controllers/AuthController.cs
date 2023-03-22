0a5900f2a1d5c139e53b81eabde60fdda61db2b3
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2023-03-22 11:04:58 -0300

    Added refresh token endpoint

--- src/UserInterface/Houston.API/Controllers/AuthController.cs
+++ src/UserInterface/Houston.API/Controllers/AuthController.cs
@@ -1,12 +1,15 @@
 ﻿using Houston.API.AuthConfigurations;
 using Houston.Application.ViewModel;
 using Houston.Core.Commands.AuthCommands;
+using Houston.Core.Entities.Redis;
 using Houston.Core.Interfaces.Repository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using MongoDB.Bson;
 using Projeta.API.Infrastructure;
 using System.Net;
+using System.Text.Json;
 
 namespace Houston.API.Controllers {
 	[Route("api/[controller]")]
@@ -24,12 +27,20 @@ namespace Houston.API.Controllers {
 			_cache = cache ?? throw new ArgumentNullException(nameof(cache));
 		}
 
-
+		/// <summary>
+		/// Performs user authentication
+		/// </summary>
+		/// <param name="command">JSON containing authentication fields</param>
+		/// <returns>A JWT with refresh token to send in header for authorization from API endpoints</returns>
+		/// <response code="200">Return a JWT</response>
+		/// <response code="403">If user is inactive or with invalid credentials</response>
 		[HttpPost]
 		[AllowAnonymous]
+		[ProducesResponseType(typeof(BearerTokenViewModel), (int)HttpStatusCode.OK)]
+		[ProducesResponseType(typeof(MessageViewModel), (int)HttpStatusCode.Forbidden)]
 		public async Task<IActionResult> SignIn([FromBody] GeneralSignInCommand command) {
 			var user = await _unitOfWork.UserRepository.FindByEmail(command.Email);
-			
+
 			// TODO: Mudar para validar senha criptografada
 			if (user is null || user.Password != command.Password) {
 				return StatusCode((int)HttpStatusCode.Forbidden, new MessageViewModel("invalidCredentials"));
@@ -40,6 +51,46 @@ namespace Houston.API.Controllers {
 			}
 
 			return Ok(await TokenService.GenerateToken(user, _signingConfigurations, _tokenConfigurations, _cache));
+		}
+
+		/// <summary>
+		/// Refreshes user JWT token
+		/// </summary>
+		/// <param name="token">Refresh token given with JWT on authentication</param>
+		/// <returns>A new JWT and refresh token</returns>
+		/// <response code="200">Return a new JWT with refresh token</response>
+		/// <response code="403">If token or user are invalid</response>
+		[HttpPost("{token}")]
+		[AllowAnonymous]
+		[ProducesResponseType(typeof(BearerTokenViewModel), (int)HttpStatusCode.OK)]
+		[ProducesResponseType(typeof(MessageViewModel), (int)HttpStatusCode.Forbidden)]
+		public async Task<IActionResult> RefreshToken(string token) {
+			if (string.IsNullOrWhiteSpace(token)) {
+				return StatusCode((int)HttpStatusCode.Forbidden, new MessageViewModel("invalidToken"));
+			}
+
+			string? redisToken = await _cache.GetStringAsync(token);
+			if (redisToken is null) {
+				return StatusCode((int)HttpStatusCode.Forbidden, new MessageViewModel("tokenExpired"));
+			}
+
+			RefreshTokenData? tokenData = JsonSerializer.Deserialize<RefreshTokenData>(redisToken);
+			if (tokenData is null) {
+				return StatusCode((int)HttpStatusCode.Forbidden, new MessageViewModel("tokenExpired"));
+			}
+
+			var user = await _unitOfWork.UserRepository.FindByIdAsync(ObjectId.Parse(tokenData.UserId));
+			if (user is null) {
+				return StatusCode((int)HttpStatusCode.Forbidden, new MessageViewModel("userNotFound"));
+			}
+
+			if (!user.IsActive) {
+				return StatusCode((int)HttpStatusCode.Forbidden, new MessageViewModel("userInactive"));
+			}
+
+			await _cache.RemoveAsync(token);
+
+			return Ok(TokenService.GenerateToken(user, _signingConfigurations, _tokenConfigurations, _cache));
 		}
 	}
 }
