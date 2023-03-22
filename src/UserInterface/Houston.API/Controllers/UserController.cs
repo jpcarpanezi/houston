3a5ebf3a44bad1847b85c6d39b1c89affcf1d0e3
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2023-03-22 17:09:59 -0300

    Added IsFirstAccess endpoint

---
+++ src/UserInterface/Houston.API/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Houston.Core.Interfaces.Repository;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Houston.API.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase {
+		private readonly IUnitOfWork _unitOfWork;
+		private readonly IDistributedCache _cache;
+		private readonly IMediator _mediator;
+
+		public UserController(IUnitOfWork unitOfWork, IDistributedCache cache, IMediator mediator) {
+			_unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+			_cache = cache ?? throw new ArgumentNullException(nameof(cache));
+			_mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+		}
+
+		[HttpGet("is-first-access")]
+		public async Task<IActionResult> IsFirstAccess() {
+			var configurations = await _cache.GetStringAsync("configurations");
+
+			if (configurations is null) { 
+				return NotFound();
+			}
+
+			return Ok();
+		}
+	}
+}
