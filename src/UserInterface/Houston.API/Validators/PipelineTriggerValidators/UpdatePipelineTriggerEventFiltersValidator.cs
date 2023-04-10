6b3c49ca8fbf4bbec40bce3eba73107d2aad7e84
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2023-04-10 14:10:16 -0300

    Added FluentValidations to UpdatePipelineTriggerCommand

---
+++ src/UserInterface/Houston.API/Validators/PipelineTriggerValidators/UpdatePipelineTriggerEventFiltersValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+using static Houston.Core.Commands.PipelineTriggerCommands.UpdatePipelineTriggerCommand;
+
+namespace Houston.API.Validators.PipelineTriggerValidators {
+	public class UpdatePipelineTriggerEventFiltersValidator : AbstractValidator<UpdatePipelineTriggerEventFilters> {
+		public UpdatePipelineTriggerEventFiltersValidator() {
+			RuleForEach(x => x.FilterValues)
+				.NotEmpty().NotNull().WithMessage(ValidatorsModelErrorMessages.NullOrEmpty);
+		}
+	}
+}
---
+++ src/UserInterface/Houston.API/Validators/PipelineTriggerValidators/UpdatePipelineTriggerEventsValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+using static Houston.Core.Commands.PipelineTriggerCommands.UpdatePipelineTriggerCommand;
+
+namespace Houston.API.Validators.PipelineTriggerValidators {
+	public class UpdatePipelineTriggerEventsValidator : AbstractValidator<UpdatePipelineTriggerEvents> {
+		public UpdatePipelineTriggerEventsValidator() {
+			RuleForEach(x => x.EventFilters).SetValidator(new UpdatePipelineTriggerEventFiltersValidator());
+		}
+	}
+}
