ba2ab1e7b60542de614f1a545f3d77831dd784a6
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2023-06-06 10:53:13 -0300

    Implemented pipeline details

--- src/Web/WebAngular/src/app/presentation/view/pages/pipeline/pipeline-details/pipeline-details.component.ts
+++ src/Web/WebAngular/src/app/presentation/view/pages/pipeline/pipeline-details/pipeline-details.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { PipelineUseCaseInterface } from 'src/app/domain/interfaces/use-cases/pipeline-use-case.interface';
+import { PipelineViewModel } from 'src/app/domain/view-models/pipeline.view-model';
+import { Toast } from 'src/app/infra/helpers/toast';
+import Swal from 'sweetalert2';
 
 @Component({
 	selector: 'app-pipeline-details',
 	templateUrl: './pipeline-details.component.html',
 	styleUrls: ['./pipeline-details.component.css']
 })
-export class PipelineDetailsComponent {
-	public isLoading: boolean = false;
+export class PipelineDetailsComponent implements OnInit {
+	public isLoading: boolean = true;
 	public pipelineDetailsForm: FormGroup = this.initializePipelineDetailsForm();
+	public pipelineId: string | null = null;
 
 	constructor(
-		private fb: FormBuilder
+		private fb: FormBuilder,
+		private route: ActivatedRoute,
+		private pipelineUseCase: PipelineUseCaseInterface
 	) { }
+
+	ngOnInit(): void {
+		this.pipelineId = this.route.snapshot.paramMap.get("id");
+
+		if (this.pipelineId)
+			this.getPipelineDetails(this.pipelineId);
+	}
 
 	private initializePipelineDetailsForm(): FormGroup {
 		return this.pipelineDetailsForm = this.fb.group({
@@ -26,7 +41,30 @@ export class PipelineDetailsComponent {
 		});
 	}
 
-	savePipelineDetails(): void {
+	getPipelineDetails(id: string): void {
+		this.pipelineUseCase.get(id).subscribe({
+			next: (response: PipelineViewModel) => this.pipelineDetailsForm.patchValue(response),
+			error: () => Swal.fire("Error", "An error has occurred while getting the pipeline details", "error")
+		}).add(() => this.isLoading = false);
+	}
 
+	savePipelineDetails(): void {
+		if (this.pipelineDetailsForm.invalid) return;
+		this.pipelineDetailsForm.disable();
+		this.isLoading = true;
+
+		this.pipelineDetailsForm.value["id"] = this.pipelineId;
+		this.pipelineUseCase.update(this.pipelineDetailsForm.value).subscribe({
+			next: () => {
+				Toast.fire({
+					icon: "success",
+					title: "Saved successfully"
+				});
+			},
+			error: () => Swal.fire("Error", "An error has occurred while saving the pipeline details", "error")
+		}).add(() => {
+			this.pipelineDetailsForm.enable();
+			this.isLoading = false;
+		});
 	}
 }
--- src/Web/WebAngular/src/app/presentation/view/pages/pipeline/pipeline.component.ts
+++ src/Web/WebAngular/src/app/presentation/view/pages/pipeline/pipeline.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { PipelineDetailsComponent } from './pipeline-details/pipeline-details.component';
 
 @Component({
 	selector: 'app-pipeline',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
 	styleUrls: ['./pipeline.component.css']
 })
 export class PipelineComponent {
+	@ViewChild("pipelineDetails") public pipelineDetails?: PipelineDetailsComponent;
 
+	constructor() { }
+
+	savePipeline(): void {
+		this.pipelineDetails?.pipelineDetailsForm.markAllAsTouched();
+
+		if (this.pipelineDetails?.pipelineDetailsForm.invalid) return;
+
+		this.pipelineDetails?.savePipelineDetails();
+	}
 }
