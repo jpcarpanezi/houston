7c446b2a1cb6eac386c67182d0eefd4e792b1796
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2023-06-07 13:40:16 -0300

    Implemented integration with pipeline trigger API endpoint

--- src/Web/WebAngular/src/app/presentation/view/pages/pipeline/pipeline.component.ts
+++ src/Web/WebAngular/src/app/presentation/view/pages/pipeline/pipeline.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { PipelineDetailsComponent } from './pipeline-details/pipeline-details.component';
+import { PipelineTriggerComponent } from './pipeline-trigger/pipeline-trigger.component';
 
 @Component({
 	selector: 'app-pipeline',
@@ -8,14 +9,18 @@ import { PipelineDetailsComponent } from './pipeline-details/pipeline-details.co
 })
 export class PipelineComponent {
 	@ViewChild("pipelineDetails") public pipelineDetails?: PipelineDetailsComponent;
+	@ViewChild("pipelineTrigger") public pipelineTrigger?: PipelineTriggerComponent;
 
 	constructor() { }
 
 	savePipeline(): void {
 		this.pipelineDetails?.pipelineDetailsForm.markAllAsTouched();
+		this.pipelineTrigger?.pipelineTriggerForm.markAllAsTouched();
 
 		if (this.pipelineDetails?.pipelineDetailsForm.invalid) return;
+		if (this.pipelineTrigger?.pipelineTriggerForm.invalid) return;
 
 		this.pipelineDetails?.savePipelineDetails();
+		this.pipelineTrigger?.savePipelineTrigger();
 	}
 }
