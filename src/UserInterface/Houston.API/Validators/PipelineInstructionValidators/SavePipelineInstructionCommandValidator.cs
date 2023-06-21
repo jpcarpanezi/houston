8eb969553c1e89e66c00b6d7c396196224bdf96e
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2023-06-21 10:09:07 -0300

    Hotfix: SavePipelineInstructionCommand not allowing single instruction

--- src/UserInterface/Houston.API/Validators/PipelineInstructionValidators/SavePipelineInstructionCommandValidator.cs
+++ src/UserInterface/Houston.API/Validators/PipelineInstructionValidators/SavePipelineInstructionCommandValidator.cs
@@ -7,7 +7,7 @@ namespace Houston.API.Validators.PipelineInstructionValidators {
 			RuleFor(x => x.PipelineInstructions)
 				.NotNull().NotEmpty().WithMessage(ValidatorsModelErrorMessages.NullOrEmpty)
 				.Must(x => x.OrderBy(y => y.ConnectedToArrayIndex).First().ConnectedToArrayIndex == null).WithMessage("The first instruction in the pipeline must not have any connections.")
-				.Must(x => x.OrderBy(y => y.ConnectedToArrayIndex).Skip(1).Select((x, i) => x.ConnectedToArrayIndex == i).Contains(true)).WithMessage("The pipeline instructions are not connected in the correct order.");
+				.Must(x => x.Count == 1 || x.OrderBy(y => y.ConnectedToArrayIndex).Skip(1).Select((x, i) => x.ConnectedToArrayIndex == i).Contains(true)).WithMessage("The pipeline instructions are not connected in the correct order.");
 
 			RuleForEach(x => x.PipelineInstructions).SetValidator(new PipelineInstructionValidator());
 		}
