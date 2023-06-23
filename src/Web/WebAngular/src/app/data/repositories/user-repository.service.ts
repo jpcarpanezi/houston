be83a4d0e291d49160deb6c1430d3bd8cf903fc1
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2023-06-23 20:40:12 -0300

    Added user commands and requests

--- src/Web/WebAngular/src/app/data/repositories/user-repository.service.ts
+++ src/Web/WebAngular/src/app/data/repositories/user-repository.service.ts
@@ -2,8 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CreateFirstSetupCommand } from 'src/app/domain/commands/user-commands/create-first-setup.command';
+import { CreateUserCommand } from 'src/app/domain/commands/user-commands/create-user.command';
 import { UpdateFirstAccessPasswordCommand } from 'src/app/domain/commands/user-commands/update-first-access-password.command';
+import { UpdatePasswordCommand } from 'src/app/domain/commands/user-commands/update-password.command';
 import { UserRepositoryInterface } from 'src/app/domain/interfaces/repositories/user-repository.interface';
+import { PaginatedItemsViewModel } from 'src/app/domain/view-models/paginated-items.view-model';
 import { UserViewModel } from 'src/app/domain/view-models/user.view-model';
 import { environment } from 'src/environments/environment';
 
@@ -12,6 +15,22 @@ import { environment } from 'src/environments/environment';
 })
 export class UserRepositoryService implements UserRepositoryInterface {
 	constructor(private http: HttpClient) { }
+
+	getAll(pageSize: number, pageIndex: number): Observable<PaginatedItemsViewModel<UserViewModel>> {
+		return this.http.get<PaginatedItemsViewModel<UserViewModel>>(`${environment.apiUrl}/user?pageSize=${pageSize}&pageIndex=${pageIndex}}`);
+	}
+
+	create(body: CreateUserCommand): Observable<UserViewModel> {
+		return this.http.post<UserViewModel>(`${environment.apiUrl}/user`, body);
+	}
+
+	toggleStatus(id: string): Observable<any> {
+		return this.http.patch<any>(`${environment.apiUrl}/user/toggleStatus/${id}`, null);
+	}
+
+	changePassword(body: UpdatePasswordCommand): Observable<any> {
+		return this.http.patch<any>(`${environment.apiUrl}/user/changePassword`, body);
+	}
 
 	isFirstSetup(): Observable<any> {
 		return this.http.get<any>(`${environment.apiUrl}/user/isFirstSetup`);
--- src/Web/WebAngular/src/app/domain/interfaces/use-cases/user-use-case.interface.ts
+++ src/Web/WebAngular/src/app/domain/interfaces/use-cases/user-use-case.interface.ts
@@ -2,9 +2,16 @@ import { Observable } from "rxjs";
 import { UpdateFirstAccessPasswordCommand } from "../../commands/user-commands/update-first-access-password.command";
 import { UserViewModel } from "../../view-models/user.view-model";
 import { CreateFirstSetupCommand } from "../../commands/user-commands/create-first-setup.command";
+import { CreateUserCommand } from "../../commands/user-commands/create-user.command";
+import { UpdatePasswordCommand } from "../../commands/user-commands/update-password.command";
+import { PaginatedItemsViewModel } from "../../view-models/paginated-items.view-model";
 
 export abstract class UserUseCaseInterface {
 	abstract firstAccess(body: UpdateFirstAccessPasswordCommand): Observable<any>;
 	abstract firstSetup(body: CreateFirstSetupCommand): Observable<UserViewModel>;
 	abstract isFirstSetup(): Observable<any>;
+	abstract getAll(pageSize: number, pageIndex: number): Observable<PaginatedItemsViewModel<UserViewModel>>;
+	abstract create(body: CreateUserCommand): Observable<UserViewModel>;
+	abstract toggleStatus(id: string): Observable<any>;
+	abstract changePassword(body: UpdatePasswordCommand): Observable<any>;
 }
