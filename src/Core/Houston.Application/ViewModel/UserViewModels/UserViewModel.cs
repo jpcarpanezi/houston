90de3d368789efc42433adcd1bb1bfa3b2f1add1
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2023-06-23 21:30:39 -0300

    Added Id to UserViewModel

--- src/Core/Houston.Application/ViewModel/UserViewModels/UserViewModel.cs
+++ src/Core/Houston.Application/ViewModel/UserViewModels/UserViewModel.cs
@@ -2,6 +2,8 @@
 
 namespace Houston.Application.ViewModel.UserViewModels {
 	public class UserViewModel {
+		public Guid Id { get; set; }
+
 		public string Email { get; set; } = null!;
 
 		public string Name { get; set; } = null!;
