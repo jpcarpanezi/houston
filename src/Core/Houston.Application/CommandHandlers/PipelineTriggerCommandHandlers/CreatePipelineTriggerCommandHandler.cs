050e9efecd40973d87627931dfe26eb8ea0a1676
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2023-06-26 22:07:53 -0300

    Hotfix: Added prefix to key comment

--- src/Core/Houston.Application/CommandHandlers/PipelineTriggerCommandHandlers/CreatePipelineTriggerCommandHandler.cs
+++ src/Core/Houston.Application/CommandHandlers/PipelineTriggerCommandHandlers/CreatePipelineTriggerCommandHandler.cs
@@ -25,7 +25,7 @@ namespace Houston.Application.CommandHandlers.PipelineTriggerCommandHandlers {
 			}
 
 			Guid pipelineTriggerId = Guid.NewGuid();
-			var deployKeys = DeployKeysService.Create($"houston-{pipelineTriggerId}");
+			var deployKeys = DeployKeysService.Create(pipelineTriggerId.ToString());
 			var PipelineTriggerEvents = new List<PipelineTriggerEvent>();
 
 			foreach (var @event in request.Events) {
