801b4ce12b1439ecc56ab355a3e15d6dd7fd2016
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2023-08-02 18:44:29 -0300

    Feature: Added tests to ConnectorCommandHandlers

---
+++ src/Tests/Houston.API.UnitTests/HandlerTests/ConnectorCommandHandlers/GetConnectorCommandHandlerTests.cs
@@ -0,0 +1,53 @@
+﻿using Houston.Application.CommandHandlers.ConnectorCommandHandlers.Get;
+
+namespace Houston.API.UnitTests.HandlerTests.ConnectorCommandHandlers {
+	[TestFixture]
+	public class GetConnectorCommandHandlerTests {
+		private readonly Mock<IUnitOfWork> _mockUnitOfWork = new();
+		private readonly Fixture _fixture = new();
+		private GetConnectorCommandHandler _handler;
+
+		[SetUp]
+		public void SetUp() {
+			_handler = new GetConnectorCommandHandler(_mockUnitOfWork.Object);
+		}
+
+		[Test]
+		public async Task Handle_WithNotFoundConnector_ShouldReturnNotFoundObject() {
+			// Arrange
+			var command = _fixture.Create<GetConnectorCommand>();
+			_mockUnitOfWork.Setup(x => x.ConnectorRepository.GetActive(It.IsAny<Guid>())).ReturnsAsync((Connector?)null);
+
+			// Act
+			var result = await _handler.Handle(command, default);
+
+			// Assert
+			result.Should().BeOfType<ErrorResultCommand>();
+
+			var errorResult = result as ErrorResultCommand;
+			errorResult?.StatusCode.Should().Be(HttpStatusCode.NotFound);
+			errorResult?.ErrorMessage.Should().Be("The requested connector could not be found.");
+			errorResult?.ErrorCode.Should().Be("connectorNotFound");
+			errorResult?.CustomBody.Should().BeNull();
+
+		}
+
+		[Test]
+		public async Task Handle_WithValidConnector_ShouldReturnOkObject() {
+			// Arrange
+			var command = _fixture.Create<GetConnectorCommand>();
+			var connector = _fixture.Build<Connector>().OmitAutoProperties().Create();
+			_mockUnitOfWork.Setup(x => x.ConnectorRepository.GetActive(It.IsAny<Guid>())).ReturnsAsync(connector);
+
+			// Act
+			var result = await _handler.Handle(command, default);
+
+			// Assert
+			result.Should().BeOfType<SuccessResultCommand<Connector, ConnectorViewModel>>();
+
+			var successResult = result as SuccessResultCommand<Connector, ConnectorViewModel>;
+			successResult?.StatusCode.Should().Be(HttpStatusCode.OK);
+			successResult?.Response.Should().BeSameAs(connector);
+		}
+	}
+}
---
+++ src/Tests/Houston.API.UnitTests/Usings.cs
@@ -0,0 +1,13 @@
+global using NUnit.Framework;
+global using FluentAssertions;
+global using AutoFixture;
+global using Moq;
+
+global using System.Net;
+
+global using Houston.Core.Interfaces.Repository;
+global using Houston.Core.Interfaces.Services;
+global using Houston.Core.Entities.Postgres;
+
+global using Houston.Application.Results;
+global using Houston.Application.ViewModel.ConnectorViewModels;
