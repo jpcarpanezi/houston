834af37c76abca7c22b4bea2b6eb6325366880a7
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2023-08-03 21:36:39 -0300

    Feature: Added tests to AuthCommandHandlers

---
+++ src/Tests/Houston.API.UnitTests/HandlerTests/AuthCommandHandlers/RefreshTokenCommandHandlerTests.cs
@@ -0,0 +1,121 @@
+﻿using Houston.Application.CommandHandlers.AuthCommandHandlers.RefreshToken;
+
+namespace Houston.API.UnitTests.HandlerTests.AuthCommandHandlers {
+	[TestFixture]
+	public class RefreshTokenCommandHandlerTests {
+		private readonly Mock<IUnitOfWork> _mockUnitOfWork = new();
+		private readonly IDistributedCache _cache = new MemoryDistributedCache(Options.Create(new MemoryDistributedCacheOptions()));
+		private readonly Fixture _fixture = new();
+		private RefreshTokenCommandHandler _handler;
+
+		[SetUp]
+		public void SetUp() {
+			var signingConfigurations = new SigningConfigurations(File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "PrivateKey.pem")));
+			var tokenConfigurations = new TokenConfigurations("houston", "houston", 1200, 3600);
+			_handler = new RefreshTokenCommandHandler(_mockUnitOfWork.Object, signingConfigurations, tokenConfigurations, _cache);
+		}
+
+		[Test]
+		public async Task Handle_WithWhiteSpacedToken_ReturnsForbiddenObject() {
+			// Arrange
+			var command = _fixture.Build<RefreshTokenCommand>().With(x => x.Token, string.Empty).Create();
+
+			// Act
+			var result = await _handler.Handle(command, default);
+
+			// Assert
+			result.Should().BeOfType<ErrorResultCommand>();
+
+			var errorResult = result as ErrorResultCommand;
+			errorResult?.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+			errorResult?.ErrorMessage.Should().Be("Invalid token.");
+			errorResult?.ErrorCode.Should().Be("invalidToken");
+			errorResult?.CustomBody.Should().BeNull();
+		}
+
+		[Test]
+		public async Task Handle_WithExpiredToken_ReturnsForbiddenObject() {
+			// Arrange
+			var command = _fixture.Create<RefreshTokenCommand>();
+
+			// Act
+			var result = await _handler.Handle(command, default);
+
+			// Assert
+			result.Should().BeOfType<ErrorResultCommand>();
+
+			var errorResult = result as ErrorResultCommand;
+			errorResult?.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+			errorResult?.ErrorMessage.Should().Be("Token expired.");
+			errorResult?.ErrorCode.Should().Be("invalidToken");
+			errorResult?.CustomBody.Should().BeNull();
+		}
+
+		[Test]
+		public async Task Handle_WithUserNotFound_ReturnsForbiddenObject() {
+			// Arrange
+			var command = _fixture.Create<RefreshTokenCommand>();
+			var refreshTokenData = _fixture.Build<RefreshTokenData>().With(x => x.UserId, Guid.NewGuid().ToString()).Create();
+			await _cache.SetStringAsync(command.Token, JsonSerializer.Serialize(refreshTokenData));
+			_mockUnitOfWork.Setup(x => x.UserRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync((User?)null);
+
+			// Act
+			var result = await _handler.Handle(command, default);
+
+			// Assert
+			result.Should().BeOfType<ErrorResultCommand>();
+
+			var errorResult = result as ErrorResultCommand;
+			errorResult?.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+			errorResult?.ErrorMessage.Should().Be("User not found.");
+			errorResult?.ErrorCode.Should().Be("userNotFound");
+			errorResult?.CustomBody.Should().BeNull();
+		}
+
+		[Test]
+		public async Task Handle_WithInactiveUser_ReturnsForbiddenObject() {
+			// Arrange
+			var command = _fixture.Create<RefreshTokenCommand>();
+			var user = _fixture.Build<User>().OmitAutoProperties().With(x => x.Active, false).Create();
+			var refreshTokenData = _fixture.Build<RefreshTokenData>().With(x => x.UserId, Guid.NewGuid().ToString()).Create();
+			await _cache.SetStringAsync(command.Token, JsonSerializer.Serialize(refreshTokenData));
+			_mockUnitOfWork.Setup(x => x.UserRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(user);
+
+			// Act
+			var result = await _handler.Handle(command, default);
+
+			// Assert
+			result.Should().BeOfType<ErrorResultCommand>();
+
+			var errorResult = result as ErrorResultCommand;
+			errorResult?.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+			errorResult?.ErrorMessage.Should().Be("User inactive.");
+			errorResult?.ErrorCode.Should().Be("userNotFound");
+			errorResult?.CustomBody.Should().BeNull();
+		}
+
+		[Test]
+		public async Task Handle_WithValidRequest_ReturnsOkObject() {
+			// Arrange
+			var command = _fixture.Create<RefreshTokenCommand>();
+			var user = _fixture.Build<User>()
+					  .OmitAutoProperties()
+					  .With(x => x.Active, true)
+					  .With(x => x.Name, "John Doe")
+					  .With(x => x.Email, "john.doe@example.com")
+					  .Create();
+			var refreshTokenData = _fixture.Build<RefreshTokenData>().With(x => x.UserId, Guid.NewGuid().ToString()).Create();
+			await _cache.SetStringAsync(command.Token, JsonSerializer.Serialize(refreshTokenData));
+			_mockUnitOfWork.Setup(x => x.UserRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(user);
+
+			// Act
+			var result = await _handler.Handle(command, default);
+
+			// Assert
+			result.Should().BeOfType<SuccessResultCommand<BearerTokenViewModel, BearerTokenViewModel>>();
+
+			var successResult = result as SuccessResultCommand<BearerTokenViewModel, BearerTokenViewModel>;
+			successResult?.StatusCode.Should().Be(HttpStatusCode.OK);
+		}
+	}
+}
--- src/Tests/Houston.API.UnitTests/Usings.cs
+++ src/Tests/Houston.API.UnitTests/Usings.cs
@@ -4,8 +4,12 @@ global using AutoFixture;
 global using Moq;
 
 global using Microsoft.AspNetCore.Http;
+global using Microsoft.Extensions.Caching.Distributed;
+global using Microsoft.Extensions.Caching.Memory;
+global using Microsoft.Extensions.Options;
 
 global using System.Net;
+global using System.Text.Json;
 
 global using EventBus.EventBus.Abstractions;
 global using EventBus.EventBus.Events;
@@ -13,8 +17,10 @@ global using EventBus.EventBus.Events;
 global using Houston.Core.Interfaces.Repository;
 global using Houston.Core.Interfaces.Services;
 global using Houston.Core.Entities.Postgres;
+global using Houston.Core.Entities.Redis;
 
 global using Houston.Application.Results;
+global using Houston.Application.Security;
 global using Houston.Application.ViewModel;
 global using Houston.Application.ViewModel.ConnectorViewModels;
 global using Houston.Application.ViewModel.ConnectorFunctionViewModels;
