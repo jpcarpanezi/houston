e7e6b3f56ca74d9cd133830605bec7ef10cbeda4
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2023-08-23 19:45:39 -0300

    feat: run pipeline now receives files from gRPC

--- src/Runner/app/proto/houston/v1/pipeline/RunPipelineRequest.ts
+++ src/Runner/app/proto/houston/v1/pipeline/RunPipelineRequest.ts
@@ -2,9 +2,11 @@
 
 
 export interface RunPipelineRequest {
+	'files'?: (Buffer | Uint8Array | string);
 	'scripts'?: (string)[];
 }
 
 export interface RunPipelineRequest__Output {
+	'files': (Buffer);
 	'scripts': (string)[];
 }
--- src/Runner/app/services/pipeline.ts
+++ src/Runner/app/services/pipeline.ts
@@ -8,13 +8,39 @@ import { BuildConnectorFunctionResponse } from "../proto/houston/v1/pipeline/Bui
 import tar from "tar";
 import fs from "fs";
 import { promisify } from "util";
+import { randomUUID } from "crypto";
+
+const OUTPUT_FOLDER = process.env.SCRIPTS_FOLDER || "app/scripts";
 
 export default class PipelineService {
-	public runPipeline(call: ServerUnaryCall<RunPipelineRequest__Output, RunPipelineResponse>, callback: sendUnaryData<RunPipelineResponse>): void {
+	public async runPipeline(call: ServerUnaryCall<RunPipelineRequest__Output, RunPipelineResponse>, callback: sendUnaryData<RunPipelineResponse>): Promise<void> {
 		let response: RunPipelineResponse = {
 			exit_code: 0,
 			instructions: []
 		};
+
+		const scriptId: string = randomUUID();
+
+		try {
+			if (!fs.existsSync(OUTPUT_FOLDER)) {
+				fs.mkdirSync(OUTPUT_FOLDER);
+			}
+
+			const outputTar = `${OUTPUT_FOLDER}/${scriptId}/${scriptId}.tar.gz`;
+			const outputTarFolder = `${OUTPUT_FOLDER}/${scriptId}`;
+
+			fs.mkdirSync(outputTarFolder, { recursive: true });
+			fs.writeFileSync(outputTar, call.request.files);
+			const extractOptions: tar.ExtractOptions & tar.FileOptions = { file: outputTar, cwd: outputTarFolder };
+			await promisify(tar.x)(extractOptions, undefined);
+		} catch (error: any) {
+			response.exit_code = error.status || 1;
+		}
+
+		if (response.exit_code != 0) {
+			callback(null, response);
+			return;
+		}
 
 		for (const script of call.request.scripts) {
 			let instruction: RunPipelineInstruction = {
@@ -24,7 +50,7 @@ export default class PipelineService {
 
 			try {
 				const options: ExecSyncOptionsWithStringEncoding = { encoding: "utf-8", stdio: "pipe", cwd: `app/scripts/` };
-				var stdout: string = execSync(`node ${script}.js`, options);
+				var stdout: string = execSync(`node ${scriptId}/${script}`, options);
 
 				instruction.stdout = stdout;
 			} catch (error: any) {
@@ -50,24 +76,23 @@ export default class PipelineService {
 		};
 
 		try {
-			const outputFolder = "app/scripts";
-			const outputTar = `${outputFolder}/index.tar.gz`;
-
-			if (!fs.existsSync(outputFolder)) {
-				fs.mkdirSync(outputFolder);
+			if (!fs.existsSync(OUTPUT_FOLDER)) {
+				fs.mkdirSync(OUTPUT_FOLDER);
 			}
 
+			const outputTar = `${OUTPUT_FOLDER}/index.tar.gz`;
+
 			fs.writeFileSync(outputTar, call.request.files);
-			const extractOptions: tar.ExtractOptions & tar.FileOptions = { file: outputTar, cwd: outputFolder };
+			const extractOptions: tar.ExtractOptions & tar.FileOptions = { file: outputTar, cwd: OUTPUT_FOLDER };
 			await promisify(tar.x)(extractOptions, undefined);
 
-			const options: ExecSyncOptionsWithStringEncoding = { encoding: "utf-8", stdio: "pipe", cwd: outputFolder };
+			const options: ExecSyncOptionsWithStringEncoding = { encoding: "utf-8", stdio: "pipe", cwd: OUTPUT_FOLDER };
 			execSync("npm install", options);
 			execSync("ncc build index.js --minify", options);
 
-			var file: Buffer = fs.readFileSync(`${outputFolder}/dist/index.js`);
+			var file: Buffer = fs.readFileSync(`${OUTPUT_FOLDER}/dist/index.js`);
 			response.dist = file;
-			response.type = this.getPackageType(outputFolder) || "commonjs";
+			response.type = this.getPackageType(OUTPUT_FOLDER) || "commonjs";
 		} catch (error: any) {
 			response.exit_code = Number(error.status) || 1;
 			response.stderr = error.stderr ? error.stderr.toString() : undefined;
