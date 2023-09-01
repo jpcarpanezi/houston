54be8dacb13a291286e5bf1bc7197595eb5fe130
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2023-08-31 21:24:15 -0300

    fix(git.ts): not creating scripts directory

--- src/Runner/app/services/git.ts
+++ src/Runner/app/services/git.ts
@@ -5,6 +5,9 @@ import { CloneRepositoryResponse } from "../proto/houston/v1/git/CloneRepository
 import fs from "fs";
 import { execSync } from "child_process";
 
+const OUTPUT_FOLDER = process.env.SCRIPTS_FOLDER || "app/scripts";
+const SSH_FOLDER = "/root/.ssh/";
+
 export default class GitService {
 	public async clone(call: ServerUnaryCall<CloneRepositoryRequest__Output, CloneRepositoryResponse>, callback: sendUnaryData<CloneRepositoryResponse>) {
 		let response: CloneRepositoryResponse = {
@@ -12,7 +15,14 @@ export default class GitService {
 		}
 
 		try {
-			fs.mkdirSync("/root/.ssh/");
+			if (!fs.existsSync(OUTPUT_FOLDER)) {
+				fs.mkdirSync(OUTPUT_FOLDER);
+			}
+
+			if (!fs.existsSync(SSH_FOLDER)) {
+				fs.mkdirSync(SSH_FOLDER);
+			}
+
 			fs.writeFileSync("/root/.ssh/id_rsa", Buffer.from(call.request.private_key, "base64").toString("utf-8"));
 			fs.chmodSync("/root/.ssh/id_rsa", 0o400);
 			execSync("ssh-keyscan -H github.com >> /root/.ssh/known_hosts");
