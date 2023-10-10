b722a21cd826953e8d5dab4619a7f212a229c3f5
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2023-10-09 21:58:53 -0300

    feat(Connectors): added support for mobile and loading

--- src/Web/WebAngular/src/app/presentation/view/pages/connectors/connectors.component.ts
+++ src/Web/WebAngular/src/app/presentation/view/pages/connectors/connectors.component.ts
@@ -19,7 +19,9 @@ export class ConnectorsComponent implements OnInit {
 	public connectors: ConnectorViewModel[] = [];
 	public activeConnector?: string;
 	public connectorFunctions: ConnectorFunctionViewModel[] = [];
-	public isLoading: boolean = true;
+
+	public isConnectorsLoading: boolean = true;
+	public isConnectorFunctionsLoading: boolean = false;
 
 	constructor(
 		private connectorUseCase: ConnectorUseCaseInterface,
@@ -29,7 +31,7 @@ export class ConnectorsComponent implements OnInit {
 	ngOnInit(): void {	}
 
 	setPage(pageInfo: any): void {
-		this.isLoading = true;
+		this.isConnectorsLoading = true;
 		this.page.pageIndex = pageInfo.currentPage == 0 ? pageInfo.currentPage : pageInfo.currentPage - 1;
 		this.page.pageSize = pageInfo.pageSize ?? 5;
 
@@ -41,18 +43,18 @@ export class ConnectorsComponent implements OnInit {
 				this.connectors = response.data;
 			},
 			error: () => Swal.fire("Error", "An error has occurred while trying to get the connectors.", "error")
-		}).add(() => this.isLoading = false);
+		}).add(() => this.isConnectorsLoading = false);
 	}
 
 	expandFunction(row: ConnectorViewModel, event: Event): void {
 		event.stopPropagation();
 		this.activeConnector = row.id;
-		this.isLoading = true;
+		this.isConnectorFunctionsLoading = true;
 
 		this.connectorFunctionUseCase.getAll(row.id, 100, 0).subscribe({
 			next: (response: PaginatedItemsViewModel<ConnectorFunctionViewModel>) => this.connectorFunctions = response.data,
 			error: () => Swal.fire("Error", "An error has occurred while trying to get the connector functions.", "error")
-		}).add(() => this.isLoading = false);
+		}).add(() => this.isConnectorFunctionsLoading = false);
 	}
 
 	deleteConnector(button: HTMLButtonElement, row: ConnectorViewModel): void {
@@ -67,20 +69,31 @@ export class ConnectorsComponent implements OnInit {
 		}).then((result) => {
 			if (result.isConfirmed) {
 				button.disabled = true;
+				this.isConnectorFunctionsLoading = true;
+				this.isConnectorsLoading = true;
 
 				this.connectorUseCase.delete(row.id).subscribe({
-					next: () => this.deleteConnectorNext(),
+					next: () => this.deleteConnectorNext(row),
 					error: () => Swal.fire("Error", "An error has occurred while trying to delete the connector.", "error")
+				}).add(() => {
+					this.isConnectorFunctionsLoading = false;
+					this.isConnectorsLoading = false;
+					button.disabled = false;
 				});
 			}
 		});
 	}
 
-	private deleteConnectorNext(): void {
+	private deleteConnectorNext(row: ConnectorViewModel): void {
 		Toast.fire({
 			icon: "success",
 			title: "Connector deleted"
 		});
+
+		if (this.activeConnector == row.id) {
+			this.activeConnector = undefined;
+			this.connectorFunctions = [];
+		}
 
 		this.setPage({ currentPage: this.page.pageIndex });
 	}
