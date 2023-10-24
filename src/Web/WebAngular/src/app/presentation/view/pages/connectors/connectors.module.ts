bb13cb4f6e99e231205322de06ca4540a800c3fa
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2023-10-23 21:44:41 -0300

    fix(Connectors): removed unused components

--- src/Web/WebAngular/src/app/presentation/view/pages/connectors/connectors.module.ts
+++ src/Web/WebAngular/src/app/presentation/view/pages/connectors/connectors.module.ts
@@ -4,19 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ConnectorsRoutingModule } from './connectors-routing.module';
 import { ConnectorsComponent } from './connectors.component';
 import { SharedModule } from '../../shared/shared.module';
-import { NewConnectorComponent } from './new-connector/new-connector.component';
-import { UpdateConnectorComponent } from './update-connector/update-connector.component';
-import { NewConnectorFunctionComponent } from './new-connector-function/new-connector-function.component';
-import { UpdateConnectorFunctionComponent } from './update-connector-function/update-connector-function.component';
 
 
 @NgModule({
 	declarations: [
-		ConnectorsComponent,
-		NewConnectorComponent,
-		UpdateConnectorComponent,
-		NewConnectorFunctionComponent,
-		UpdateConnectorFunctionComponent
+		ConnectorsComponent
 	],
 	imports: [
 		CommonModule,
