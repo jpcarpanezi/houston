2040fbc1e1b512e1e01e49cbf9cec2a40e37258b
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2023-11-16 17:58:43 -0300

    fix: changed method visibility

--- src/Core/Houston.Application/PipelineBehaviors/ValidationPreProcessorFilter.cs
+++ src/Core/Houston.Application/PipelineBehaviors/ValidationPreProcessorFilter.cs
@@ -1,6 +1,6 @@
 ﻿namespace Houston.Application.PipelineBehaviors {
 	public class ValidationPreProcessorFilter<TRequest> : IRequestPreProcessor<TRequest> where TRequest : notnull {
-		public readonly IValidator<TRequest> _validator;
+		private readonly IValidator<TRequest> _validator;
 
 		public ValidationPreProcessorFilter(IValidator<TRequest> validator) {
 			_validator = validator ?? throw new ArgumentNullException(nameof(validator));
